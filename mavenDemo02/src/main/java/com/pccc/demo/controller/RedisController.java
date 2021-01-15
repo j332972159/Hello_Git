23ee2a405d4848332e1b3dd53f0fd022919862c1
jinquanhua <j19921842048@163.com>
2021-01-15 09:19:35 +0800

    first commit

---
+++ mavenDemo02/src/main/java/com/pccc/demo/controller/RedisController.java
@@ -0,0 +1,41 @@
+package com.pccc.demo.controller;
+
+import com.pccc.demo.util.RedisUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author jinquanhua
+ * @date 2020/5/17 12:46
+ */
+@RestController
+public class RedisController {
+    @Autowired
+    private RedisUtils redisUtils;
+
+    @RequestMapping("/testRedis")
+    public String testRedis(){
+        String id="1";
+        //查询redis缓存中是否存在
+        boolean exists = redisUtils.exists(id);
+        if(exists){
+            //获取缓存
+            Object obj = redisUtils.get(id);
+            System.out.println("从缓存中获取的数据："+obj.toString());
+            return obj.toString();
+        }else{
+            System.out.println("模拟去mysql获取数据");
+            String list="test data!";
+            //数据插入缓存
+            //数据插入缓存（set中的参数含义：key值，user对象，缓存存在时间10（long类型），时间单位）
+            redisUtils.set(id,list,10L, TimeUnit.MINUTES);
+            System.out.println("数据已存入redis");
+            return list;
+        }
+    }
+
+
+}
---
+++ mavenDemo02/src/main/java/com/pccc/demo/util/StringUtil.java
@@ -0,0 +1,20 @@
+package com.pccc.demo.util;
+
+/**
+ * @Author jinquanhua
+ * @date 2020/12/25 11:03
+ */
+public class StringUtil {
+    public static void main(String[] args) {
+        String str = "a,s,d,f,g,h,,k,,";
+        String[] split = str.split(",");
+        System.out.println(split.length);
+        System.out.println(split[split.length-2]);
+        String s = split[split.length - 2];
+        if (s == null){
+            System.out.println("s is null");
+        }else if(s.equals("")){
+            System.out.println("s is \"\"");
+        }
+    }
+}
