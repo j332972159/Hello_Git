2140c8f45f131739d1907d32cd1406401ea69d8e
jinquanhua <j19921842048@163.com>
2021-04-19 17:17:45 +0800

    3 commit and push

---
+++ esdemo/src/test/java/com/pccc/es/esdemo/ElasticSearchTest.java
@@ -0,0 +1,16 @@
+package com.pccc.es.esdemo;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author jinquanhua
+ * @date 2020/6/22 19:25
+ */
+
+@SpringBootTest
+public class ElasticSearchTest {
+    @Test
+    public  void esTest() throws Exception{
+
+    }
+}
