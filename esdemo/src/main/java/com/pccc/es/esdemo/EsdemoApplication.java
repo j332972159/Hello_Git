ad7b5fd3f16436ac30f2b9319f637dfe6b28c0d1
jinquanhua <j19921842048@163.com>
2021-04-19 17:26:22 +0800

    4 commit and push

---
+++ esdemo/src/main/java/com/pccc/es/esdemo/EsdemoApplication.java
@@ -0,0 +1,15 @@
+package com.pccc.es.esdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsdemoApplication {
+    //哈哈2
+    //哈哈3
+    //哈哈4
+    public static void main(String[] args) {
+        SpringApplication.run(EsdemoApplication.class, args);
+    }
+
+}
